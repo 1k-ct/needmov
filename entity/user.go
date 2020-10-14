2a36c3d48ff9e4b35b08ee7a0495788ce5c27c6b
1k-ct <268iop@gmail.com>
2020-10-14 20:27:54 +0900

    jsonに変更

--- entity/user.go
+++ entity/user.go
@@ -6,13 +6,13 @@ import (
 
 // Users users database -> id createdat updateat deletedat name password email
 type Users struct {
-	ID        int
-	CreatedAt string
-	UpDatedAt string
-	DeletedAt string
-	Name      string
-	PassWord  string
-	Email     string
+	ID        int    `json:"id,omitempty"`
+	CreatedAt string `json:"create_at,omitempty"`
+	UpDatedAt string `json:"up_dated_at,omitempty"`
+	DeletedAt string `json:"deleted_at,omitempty"`
+	Name      string `json:"name,omitempty"`
+	PassWord  string `json:"pass_word,omitempty"`
+	Email     string `json:"email,omitempty"`
 }
 
 // UsersMig -> gorm.Model UserName Password
@@ -30,20 +30,72 @@ type SessionInfo struct {
 // VideoInfos video info
 type VideoInfos struct {
 	//ID               int
-	VideoID          string
-	VideoName        string
-	VideoDescription string `gorm:"type:text"`
-	ThumbnailURL     string
-	ViewCount        uint64 `gorm:"type:int"`
-	CommentCount     uint64 `gorm:"type:int"`
-	LikeCount        uint64 `gorm:"type:int"`
-	DislikeCount     uint64 `gorm:"type:int"`
-	UploadDate       time.Time
-	CreatedAt        time.Time
+	VideoID   string `json:"video_id,omitempty"`
+	VideoName string `json:"video_name,omitempty"`
+	//VideoDescription string    `gorm:"type:text" json:"video_description,omitempty"`
+	ThumbnailURL string    `json:"thumbnail_url,omitempty"`
+	ViewCount    uint64    `gorm:"type:int" json:"video_count,omitempty"`
+	CommentCount uint64    `gorm:"type:int" json:"comment_count,omitempty"`
+	LikeCount    uint64    `gorm:"type:int" json:"like_count,omitempty"`
+	DislikeCount uint64    `gorm:"type:int" json:"dislike_count,omitempty"`
+	UploadDate   time.Time `json:"upload_date,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
 }
 
 // ChannelInfos channel info
 type ChannelInfos struct {
+	//ID              uint64
+	ChannelID       string
+	ChannelName     string
+	ViewCount       uint64 `gorm:"type:int"`
+	SubscriberCount uint64 `gorm:"type:int"`
+	VideoCount      uint64 `gorm:"type:int"`
+	CreatedAt       time.Time
+}
+
+// ShiromiyaVideoInfos 白宮みみ
+type ShiromiyaVideoInfos struct {
+	//ID               int
+	VideoID   string `json:"video_id,omitempty"`
+	VideoName string `json:"video_name,omitempty"`
+	//VideoDescription string    `gorm:"type:text" json:"video_description,omitempty"`
+	ThumbnailURL string    `json:"thumbnail_url,omitempty"`
+	ViewCount    uint64    `gorm:"type:int" json:"video_count,omitempty"`
+	CommentCount uint64    `gorm:"type:int" json:"comment_count,omitempty"`
+	LikeCount    uint64    `gorm:"type:int" json:"like_count,omitempty"`
+	DislikeCount uint64    `gorm:"type:int" json:"dislike_count,omitempty"`
+	UploadDate   time.Time `json:"upload_date,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
+
+// ShiromiyaChannelInfos 白宮みみ
+type ShiromiyaChannelInfos struct {
+	//ID              uint64
+	ChannelID       string
+	ChannelName     string
+	ViewCount       uint64 `gorm:"type:int"`
+	SubscriberCount uint64 `gorm:"type:int"`
+	VideoCount      uint64 `gorm:"type:int"`
+	CreatedAt       time.Time
+}
+
+// HashibaVideoInfos 羽柴なつみ
+type HashibaVideoInfos struct {
+	//ID               int
+	VideoID   string `json:"video_id,omitempty"`
+	VideoName string `json:"video_name,omitempty"`
+	//VideoDescription string    `gorm:"type:text" json:"video_description,omitempty"`
+	ThumbnailURL string    `json:"thumbnail_url,omitempty"`
+	ViewCount    uint64    `gorm:"type:int" json:"video_count,omitempty"`
+	CommentCount uint64    `gorm:"type:int" json:"comment_count,omitempty"`
+	LikeCount    uint64    `gorm:"type:int" json:"like_count,omitempty"`
+	DislikeCount uint64    `gorm:"type:int" json:"dislike_count,omitempty"`
+	UploadDate   time.Time `json:"upload_date,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
+
+// HashibaChannelInfos 羽柴なつみ
+type HashibaChannelInfos struct {
 	//ID              uint64
 	ChannelID       string
 	ChannelName     string
