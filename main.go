e2e2229277b39c882dccb2b881b226a9a12d747e
1k-ct <268iop@gmail.com>
2020-10-19 17:46:54 +0900

    本番ではない

--- main.go
+++ main.go
@@ -1,65 +1,58 @@
 package main
 
 import (
+	"context"
 	"log"
 	"needmov/db"
 	"needmov/server"
+	youtubeapi "needmov/youtubeAPI"
+	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var url string = "https://virtual-youtuber.userlocal.jp/lives"
-
-func startCruise(url string) func() (string, bool) {
-	dataLink := GetLivingVideo(url) //動画をスクレイピングしてくる
-	log.Println("スクレイピング出来たよ！")
-	lenDataLink := len(dataLink) // 動画の本数
-	//fmt.Println(lenDataLink)
-	n := -1
-	return func() (string, bool) {
-		n++
-		if n == lenDataLink {
-			return dataLink[0], false //errors.New("終了")
-		}
-		return dataLink[n], true //, "mada"
-	}
-}
-
-// GetLivingVideo 指定されたLIVE配信中の動画のURLを取得する -> return slice
-func GetLivingVideo(url string) []string {
-	var dataLink []string
-	doc, _ := goquery.NewDocument(url)
-	doc.Find("div").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Attr("data-link")
-		if len(url) > 10 {
-			dataLink = append(dataLink, url)
-		}
-	})
-	return dataLink
-}
-
-// GetChannelName 羽柴チャンネルの視聴回数をスクレイピングする
-func GetChannelName() string {
-	doc, err := goquery.NewDocument("https://www.youtube.com/channel/UC_BlXOQe5OcRC7o0GX8kp8A/about")
-	if err != nil {
-		panic(err)
-	}
-	selection := doc.Find("#right-column > yt-formatted-string:nth-child(3)")
-	innerSelection := selection.Text()
-
-	return innerSelection
-}
-
 func main() {
-	//db.NewMakeDB()
-	//server.Init()
-	//id, name, viewCount, subscriberCount, videoCount := youtubeapi.PrintChannelInfo("UC_BlXOQe5OcRC7o0GX8kp8A")
-	//fmt.Println(id, name, viewCount, subscriberCount, videoCount)
-
-	//id, name, description, thumbnailURL, viewCount, commentCount, likeCount, dislikeCount, uploadDate := youtubeapi.PrintVideoInfo("pV1GoNi5mFs")
+	//id, name, description, thumbnailURL, viewCount, commentCount, likeCount, dislikeCount, uploadDate := youtubeapi.PrintVideoInfo("U1xQX6BllRA")
 	//fmt.Println(id, name, description, thumbnailURL, viewCount, commentCount, likeCount, dislikeCount, uploadDate)
+
 	db.NewMakeDB()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go periodicLoop(ctx, 60*time.Second)
 	server.Init()
+}
+
+func jsonCreateChannel(t time.Time, channelURL ...string) {
+	//var channelInfos []entity.ChannelInfos
+	//var c *gin.Context
+	//var dbd *gorm.DB
+	//dbd.NewRecord(channelInfos)
+	//if dbd.NewRecord(channelInfos) == false {
+	//	c.JSON(http.StatusOK, channelInfos)
+	//}
+	//if err := c.BindJSON(&channelInfos); err != nil {
+	//	c.String(http.StatusBadRequest, "failed err:"+err.Error())
+	//}
+	for _, url := range channelURL {
+		channelID, channelName, viewCount, subscriberCount, videoCount := youtubeapi.PrintChannelInfo(url)
+		db.InsertChannelInfo(channelID, channelName, viewCount, subscriberCount, videoCount)
+	}
+	//c.Redirect(http.StatusFound, "/")
+}
+func periodicLoop(ctx context.Context, interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	date := time.Now()
+	channelURL := []string{"UCL-2thbJ7grC9fmGF4OLuTg", "UCvUc0m317LWTTPZoBQV479A"}
+	jsonCreateChannel(date, channelURL...)
+	log.Println("go func api json create ch-info------")
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case t := <-ticker.C:
+			jsonCreateChannel(t, channelURL...)
+		}
+	}
 }
