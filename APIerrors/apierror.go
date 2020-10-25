0ce969cdcffb8d5b8749c79cf6021f05e652e392
1k-ct <268iop@gmail.com>
2020-10-25 17:52:01 +0900

    エラーメッセージ

---
+++ APIerrors/apierror.go
@@ -0,0 +1,25 @@
+package apierrors
+
+//ApplicationError エラーtype
+type ApplicationError struct {
+	Code  int    `json:"code"`
+	Level string `json:"level"`
+	Msg1  string `json:"msg1"` //英語で書く
+	Msg2  string `json:"msg2"` //日本語で書く
+}
+
+//ErrInvalidURL 無効なURLのとき。UCで始まり２４文字ではない
+var ErrInvalidURL = &ApplicationError{
+	Code:  1,
+	Level: "Error",
+	Msg1:  "This CHANNEL URL is not valid.The length of the string is not 24 characters.Or it is a string that does not begin with 'UC'.",
+	Msg2:  "このCHANNEL URLは無効です。文字列の長さが２４文字ではありません。または、”UC”　で始まらない文字列です。",
+}
+
+//ErrDuplicateURL URLが同じ場合
+var ErrDuplicateURL = &ApplicationError{
+	Code:  2,
+	Level: "Error",
+	Msg1:  "That URL is a duplicate.It's already in the DB.",
+	Msg2:  "そのURLは重複する。すでにDBにあります。",
+}
