736f34f8f0b23dee1fb8523d8b6cdd2b911bd73a
1k-ct <268iop@gmail.com>
2020-11-09 20:11:28 +0900

    合わせてもの

--- service/user_service.go
+++ service/user_service.go
@@ -29,20 +29,22 @@ func (s Service) GetAllUserInfo() ([]User, error) {
 func (s Service) GetSomeoneChannelInfo(c *gin.Context, url string) ([]ChannelInfo, error) {
 	db := db.ConnectGorm()
 	defer db.Close()
-	var channel []ChannelInfo
-	if err := db.Where("channel_id = ?", url).Find(&channel).Error; err != nil {
+	var ch []ChannelInfo
+	err := db.Where("channel_id = ?", url).Find(&ch).Error
+	if err != nil {
 		return nil, err
 	}
-	return channel, nil
+	return ch, nil
 }
 
 //GetSomeoneVideoInfo entity.VideoInfos(db)から指定したvideoidの情報を全部取る
 func (s Service) GetSomeoneVideoInfo(c *gin.Context, videoID string) ([]VideoInfos, error) {
 	db := db.ConnectGorm()
 	defer db.Close()
-	var video []VideoInfos
-	if err := db.Where("video_id = ?", videoID).Find(&video).Error; err != nil {
+	var vi []VideoInfos
+	err := db.Where("video_id = ?", videoID).Find(&vi).Error
+	if err != nil {
 		return nil, err
 	}
-	return video, nil
+	return vi, nil
 }
