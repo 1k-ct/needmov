216b04995f0ec101669129c608cb2129664b7f0a
1k-ct <268iop@gmail.com>
2020-11-23 14:33:55 +0900

    いらないも消した

--- server/server.go
+++ server/server.go
@@ -1,14 +1,9 @@
 package server
 
 import (
-	"log"
 	user "needmov/controller"
-	"needmov/entity"
 	"net/http"
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 )
@@ -22,65 +17,15 @@ func Init() {
 
 func router(gae bool) *gin.Engine {
 	r := gin.Default()
-	if !gae {
-		r.LoadHTMLGlob("/home/sato/go/src/github.com/1k-ct/nomv/src/needmov/templates/**/*") //*/**
-	} else {
-		r.Static("/assets", "./assets")
-		r.LoadHTMLGlob("templates/**/*")
-	}
-	store := cookie.NewStore([]byte("secret"))
-	r.Use(sessions.Sessions("mysession", store))
 
 	ctrl := user.Controller{}
 
-	r.GET("/", ctrl.Start)
-
-	r.GET("/ping", ctrl.Connection)
-
-	u := r.Group("/admin")
-	u.Use(sessionCheck())
-	{
-		u.GET("/", ctrl.Adimn)
-	}
-
-	r.GET("/signup", ctrl.SignUpGet)
-	r.POST("/signup", ctrl.SignUpPost)
-	r.GET("/login", ctrl.LoginGet)
-	r.POST("/login", ctrl.LoginPost)
-
-	hashiba := r.Group("/hashiba")
-	{
-		hashiba.GET("/", ctrl.HashibaHome)
-		hashiba.GET("/reg", ctrl.HashibaDeteil)
-	}
-
-	shiromiya := r.Group("/shiromiya")
-	{
-		shiromiya.GET("/", ctrl.ShiromiyaHome)
-		shiromiya.GET("/reg", ctrl.ShiromiyaRegVideo)
-	}
-
-	r.GET("/logout", ctrl.PostLogout)
-	r.POST("/regvideo", ctrl.CreateVideoInfo)
-	r.POST("/regchannel", ctrl.CreateChannelInfo)
-	r.POST("/shiromiyaregvideo", ctrl.ShiromiyaCreateVideoInfo)
-	r.POST("/shiromiyaregchannel", ctrl.ShiromiyaCreateChannelInfo)
-	r.POST("/hashibaregvideo", ctrl.HashibaCreateVideoInfo)
-	r.POST("/hashibaregchannel", ctrl.HashibaCreateChannelInfo)
-
-	r.GET("/ggnew", ctrl.RedirectGGNew)
-	r.GET("/stoppoint", ctrl.Stoppoint)
-
-	sc := startCruise(url)
-	r.GET("/new", func(c *gin.Context) {
-		dataLink, ok := sc()
-		if ok {
-			c.HTML(200, "index.html", gin.H{"dataLink": dataLink})
-		} else if !ok {
-			sc = startCruise(url)
-			c.Redirect(302, "/ggnew")
-		}
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "name_sel.html", nil)
 	})
+	// r.POST("/", ctrl.GetLikeAMsg)
+
 	api := r.Group("/api")
 	{
 		//"api/ch-info" apiで登録したデータベースを全部取る "api/ch-info"
@@ -115,88 +60,37 @@ func router(gae bool) *gin.Engine {
 			comme.POST("/data", ctrl.APIInsertCommentData)
 
 			// name ? その人(name)が書いたコメント、チャンネル内全て
-			// api/comme/namesel?name=xxx
+			// api/comme/name_sel?name=xxx
 			comme.GET("/name_sel", ctrl.CommeName)
 
 			// name ? and video_id ? その人(name)が書いたコメント、動画内全て
-			// api/comme/namecommesel?name=xxx&id=xxx(url)
+			// api/comme/namecomme_sel?name=xxx&id=xxx(url)
 			comme.GET("/namecomme_sel", ctrl.CommeNameCom)
 
 			// type ? (superChat) そのチャンネルのsuperChat全て
-			// api/comme/allsc
+			// api/comme/all_sc
 			comme.GET("/all_sc", ctrl.CommeAllSC) // もし、他のチャンネルが登録されれば消す
 
 			// type ? (superChat) and video_id(url) その動画内でのsuperChat全て
-			// api/comme/videosc?chid=xxx&id=xxx
+			// api/comme/video_sc?chid=xxx&id=xxx
 			comme.GET("video_sc", ctrl.CommeVideoSC)
 
 			// type ? (superChat) and name ? その人(name)のsuperChatチャンネル内全て
-			// api/comme/namesc?name=xxx&chid=xxx
+			// api/comme/name_sc?name=xxx&chid=xxx
 			comme.GET("/name_sc", ctrl.CommeNameSC)
 
 			// type ? (superChat) and name ? video_id(url) ?　その人(name)のsuperChat動画内全て
-			// api/comme/namesc?name=xxx&id=xxx&chid=xxx
+			// api/comme/namevideo_sc?name=xxx&id=xxx&chid=xxx
 			comme.GET("/namevideo_sc", ctrl.CommeNameVideoSC)
 
 			// message ? (like) そのチャンネルでコメントを検索、全て
-			// api/comme/chmsg?chid=xxx&msg=xxx
+			// api/comme/chmsg_simi?chid=xxx&msg=xxx
 			comme.GET("/chmsg_simi", ctrl.CommeChMsg)
 			// message ? (like) video_id ? その動画内でのコメント検索、全て
-			// api/comme/chvimsg?chid=xxx&id=xxx&msg=xxx
+			// api/comme/chvimsg_simi?chid=xxx&id=xxx&msg=xxx
 			comme.GET("/chvimsg_simi", ctrl.CommeChViMsg)
 
 		}
 	}
-
 	return r
-}
-
-// LoginInfo cookie 関係
-var LoginInfo entity.SessionInfo
-
-func sessionCheck() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		session := sessions.Default(c)
-		LoginInfo.ID = session.Get("ID")
-
-		// セッションがない場合、ログインフォームをだす
-		if LoginInfo.ID == nil {
-			log.Println("ログインしていません")
-			c.Redirect(http.StatusMovedPermanently, "/")
-			c.Abort()
-		} else {
-			c.Set("ID", LoginInfo.ID)
-			c.Next()
-		}
-		log.Println("ログインチェック終わり")
-	}
-}
-
-var url string = "https://virtual-youtuber.userlocal.jp/lives"
-
-func startCruise(url string) func() (string, bool) {
-	dataLink := GetLivingVideo(url)
-	lenDataLink := len(dataLink) // lenDataLink = 動画の本数
-	n := -1
-	return func() (string, bool) {
-		n++
-		if n == lenDataLink {
-			return dataLink[0], false //errors.New("終了")
-		}
-		return dataLink[n], true
-	}
-}
-
-// GetLivingVideo 指定されたLIVE配信中の動画のURLを取得する -> return slice
-func GetLivingVideo(url string) []string {
-	var dataLink []string
-	doc, _ := goquery.NewDocument(url)
-	doc.Find("div").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Attr("data-link")
-		if len(url) > 10 {
-			dataLink = append(dataLink, url)
-		}
-	})
-	return dataLink
 }
