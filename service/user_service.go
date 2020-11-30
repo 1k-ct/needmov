38c3ce8e9eae4b7c726629c1867a63f6a15d953f
1k-ct <268iop@gmail.com>
2020-11-30 16:54:30 +0900

    誰かのチャンネル情報の取得

--- service/user_service.go
+++ service/user_service.go
@@ -9,10 +9,7 @@ import (
 
 type Service struct{}
 
-// User struct db Users
-type User entity.Users
 type ChannelInfo entity.ChannelInfos
-type VideoInfos entity.VideoInfos
 
 //GetSomeoneChannelInfo entity.ChannelInfos(db)から指定したURLの情報を全部取る
 func (s Service) GetSomeoneChannelInfo(c *gin.Context, url string) ([]ChannelInfo, error) {
@@ -24,16 +21,4 @@ func (s Service) GetSomeoneChannelInfo(c *gin.Context, url string) ([]ChannelInf
 		return nil, err
 	}
 	return ch, nil
-}
-
-//GetSomeoneVideoInfo entity.VideoInfos(db)から指定したvideoidの情報を全部取る
-func (s Service) GetSomeoneVideoInfo(c *gin.Context, videoID string) ([]VideoInfos, error) {
-	db := db.ConnectGorm()
-	defer db.Close()
-	var vi []VideoInfos
-	err := db.Where("video_id = ?", videoID).Find(&vi).Error
-	if err != nil {
-		return nil, err
-	}
-	return vi, nil
 }
