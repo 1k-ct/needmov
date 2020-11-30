08ab56ffcc9cb158c14d3a05c0dde4425e90c994
1k-ct <268iop@gmail.com>
2020-11-30 16:56:36 +0900

    そんないらん

--- db/dbAPI.go
+++ db/dbAPI.go
@@ -1,35 +1,5 @@
 package db
 
-import (
-	"needmov/entity"
-	"time"
-)
-
-// InsertVideoInfo videoInfo db に、情報を書き込み
-func InsertVideoInfo(
-	videoID string,
-	videoName string,
-	thumbnailURL string,
-	viewCount uint64,
-	commentCount uint64,
-	likeCount uint64,
-	dislikeCount uint64,
-	uploadDate time.Time,
-) {
-	db := ConnectGorm()
-	db.Create(&entity.VideoInfos{
-		VideoID:      videoID,
-		VideoName:    videoName,
-		ThumbnailURL: thumbnailURL,
-		ViewCount:    viewCount,
-		CommentCount: commentCount,
-		LikeCount:    likeCount,
-		DislikeCount: dislikeCount,
-		UploadDate:   uploadDate,
-	})
-	defer db.Close()
-}
-
 // CreateDBSelfFn DB Insert create 自作関数
 // var d Data
 // db := db.ConnectGorm()
@@ -42,25 +12,4 @@ func CreateDBSelfFn(d interface{}) error {
 		return err
 	}
 	return nil
-}
-
-// GetDBVideoInfo databaseから、Video_Infoの情報を取得
-func GetDBVideoInfo() ([]entity.VideoInfos, error) {
-	db := ConnectGorm()
-	var videoInfo []entity.VideoInfos
-	err := db.Find(&videoInfo).Error
-	if err != nil {
-		return nil, err
-	}
-	db.Close()
-	return videoInfo, nil
-}
-
-// DeleteDBVideoInfo 選択したidをVideoInfo DB　から削除
-func DeleteDBVideoInfo(id int) {
-	db := ConnectGorm()
-	var videoInfo entity.VideoInfos
-	db.Find(&videoInfo, id)
-	db.Delete(&videoInfo)
-	db.Close()
 }
